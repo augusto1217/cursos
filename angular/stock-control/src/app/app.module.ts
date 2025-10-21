cf127be296ff0f691d06958812fe368f4ed8df73
Osandi Augusto <augusto1217@gmail.com>
2025-10-20 23:13:48 -0300

    Implementando o formulário de login

--- angular/stock-control/src/app/app.module.ts
+++ angular/stock-control/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ToastModule } from "primeng/toast"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
+import { RippleModule } from "primeng/ripple";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -27,8 +28,9 @@ import { HomeComponent } from './modules/home/home.component';
     CardModule,
     InputTextModule,
     ButtonModule,
-    ToastModule
-  ],
+    ToastModule,
+    RippleModule
+],
   providers: [],
   bootstrap: [AppComponent],
 })
