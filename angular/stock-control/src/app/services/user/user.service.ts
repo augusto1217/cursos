3ba8896098225a68b2ea9a643bde4353d72639c6
Osandi Augusto <augusto1217@gmail.com>
2025-10-21 22:40:30 -0300

    Criando estrutura para login e autenticação

---
+++ angular/stock-control/src/app/services/user/user.service.ts
@@ -0,0 +1,30 @@
+import { AuthRequest } from './../../models/interfaces/user/auth/AuthRequest';
+import { AuthResponse } from './../../models/interfaces/user/auth/AuthResponse';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { enviroment } from 'src/enviroments/enviroments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private API_URL = enviroment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  signupUser(requestData: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`,
+      requestData
+    );
+  }
+
+  authUser(requestData: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestData);
+  }
+}
