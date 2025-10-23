0c7942b8b4e66aaa5bbf3e01755aca855c9a6bba
Osandi Augusto <augusto1217@gmail.com>
2025-10-22 21:50:00 -0300

    Adicionando ao projeto a dependência ngx-cookie-service

--- angular/stock-control/src/app/app.module.ts
+++ angular/stock-control/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CookieService } from "ngx-cookie-service"
 
 import { CardModule } from "primeng/card"
 import { InputTextModule } from "primeng/inputtext"
 import { ButtonModule } from "primeng/button"
 import { ToastModule } from "primeng/toast"
-
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,8 +29,9 @@ import { RippleModule } from "primeng/ripple";
     ButtonModule,
     ToastModule,
     RippleModule
+
 ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
