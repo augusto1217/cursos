17438f999954c244fc3b3e21c606154b8d7b47d8
Osandi Augusto <augusto1217@gmail.com>
2025-10-22 21:48:33 -0300

    Removendo import desnecessário

--- angular/stock-control/src/app/services/user/user.service.ts
+++ angular/stock-control/src/app/services/user/user.service.ts
@@ -3,8 +3,6 @@ import { AuthResponse } from './../../models/interfaces/user/auth/AuthResponse';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
-import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
 import { enviroment } from 'src/enviroments/enviroments';
