5ab98fd1dca09c621a2fea0f5dc2d96578a3c689
Augusto Mariano <augusto1217@gmail.com>
2025-10-25 18:03:16 -0300

    Criando Projeto para medir temperatura de cidade

---
+++ angular/weather-app/src/app/modules/wheather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherData';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+
+  @Input() weatherDataInput!: WeatherDatas;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+}
