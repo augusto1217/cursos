a1c99a3832331fd40aa0f6df503871b6b45fdc35
Augusto Mariano <augusto1217@gmail.com>
2025-10-27 21:40:20 -0300

    Criando modulo de dashboar e aprendendo sobre lazyload ou loadlazy

--- angular/stock-control/src/app/app-routing.module.ts
+++ angular/stock-control/src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/page/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboad',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'dashboad',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then(
+      (m) => m.DashboardModule
+    )
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
